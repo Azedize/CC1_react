b2ca65f5e067dd88a7799d926648327676862328
azedine chentouf <azedinechentouf0@gmail.com>
2023-11-17 23:05:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Routes, Route } from 'react-router-dom'
+import Calc from './Compenent/Calc';
+import { TodoWrapper } from './Compenent/TodoWrapper';
+import Slider from './Compenent/Slider';
+import CollapsibleExample from './Compenent/Navbar';
+import Comp4 from './Compenent/Comp4'
+import './style_Clc.css'
+import Contenu from "./Components/Contenu/Contenu";
+import ToggleLangs from "./Components/ToggleLangs/ToggleLangs";
+import ContextProvider from "./context/langContext";
 function App() {
   return (
+    <ContextProvider >
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><CollapsibleExample /><br /></h1>
+
+      <Routes>
+        <Route path="/" element={<Calc />} />
+        <Route path="/todo" element={<TodoWrapper />} />
+        <Route path="/slider" element={<Slider />} />
+        <Route path="/pagination" element={<Comp4 />} />
+        <Route path="/lang" element={<>  <ToggleLangs/><Contenu /></>} />
+        <Route path="*" element={<h1 className='h1'>404</h1>} />
+      </Routes>
     </div>
+    </ContextProvider >
   );
 }
 
